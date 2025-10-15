a03d5191174c53b10106ab6b2f6b1abd58458516
krishnaardhapurkar17 <ardhapurkarkrishna@gmail.com>
2025-10-15 13:09:56 -0400

    Implement Django models, admin, and get_cars API

--- xrwvm-fullstack_developer_capstone/server/djangoapp/models.py
+++ xrwvm-fullstack_developer_capstone/server/djangoapp/models.py
@@ -1,25 +1,56 @@
-# Uncomment the following imports before adding the Model code
-
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    # Any other fields you would like to include in car make model
+
+    def __str__(self):
+        # Return the name as the string representation
+        return self.name
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+class CarModel(models.Model):
+    # Many-To-One relationship to Car Make model
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    
+    # Type (CharField with a choices argument)
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    COUPE = 'Coupe'
+    HATCHBACK = 'Hatchback'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (COUPE, 'Coupe'),
+        (HATCHBACK, 'Hatchback')
+    ]
+    type = models.CharField(
+        max_length=20,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+
+    # Year (IntegerField with min/max value validators)
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015)
+        ]
+    )
+    # Any other fields you would like to include in car model
+
+    def __str__(self):
+        # Return the car make name and the model name
+        return f"{self.car_make.name} {self.name}"

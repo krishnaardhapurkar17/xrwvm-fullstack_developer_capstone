aa16a8b4ee923f18abe6134461fa68439fbd77c7
krishnaardhapurkar17 <ardhapurkarkrishna@gmail.com>
2025-10-16 15:51:48 +0530

    properly formated

--- xrwvm-fullstack_developer_capstone/server/djangoapp/models.py
+++ xrwvm-fullstack_developer_capstone/server/djangoapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
-from django.utils.timezone import now
+
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
+
 
 # <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
@@ -21,33 +23,27 @@ class CarModel(models.Model):
     # Many-To-One relationship to Car Make model
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    
+
     # Type (CharField with a choices argument)
-    SEDAN = 'Sedan'
-    SUV = 'SUV'
-    WAGON = 'Wagon'
-    COUPE = 'Coupe'
-    HATCHBACK = 'Hatchback'
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "Wagon"
+    COUPE = "Coupe"
+    HATCHBACK = "Hatchback"
     CAR_TYPES = [
-        (SEDAN, 'Sedan'),
-        (SUV, 'SUV'),
-        (WAGON, 'Wagon'),
-        (COUPE, 'Coupe'),
-        (HATCHBACK, 'Hatchback')
+        (SEDAN, "Sedan"),
+        (SUV, "SUV"),
+        (WAGON, "Wagon"),
+        (COUPE, "Coupe"),
+        (HATCHBACK, "Hatchback"),
     ]
-    type = models.CharField(
-        max_length=20,
-        choices=CAR_TYPES,
-        default=SEDAN
-    )
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default=SEDAN)
 
     # Year (IntegerField with min/max value validators)
     year = models.IntegerField(
-        default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ]
+        default=2023, validators=[
+            MaxValueValidator(2023), MinValueValidator(2015)
+            ]
     )
     # Any other fields you would like to include in car model
 
